46ab800015bc72fdea2c7e1b1690639c05b389ec
ii4elka <konovalenko.it@gmail.com>
2021-07-30 00:47:13 +0300

    overlay

--- project_3/src/js/script.js
+++ project_3/src/js/script.js
@@ -29,11 +29,11 @@ document.querySelector('.slick_next').addEventListener('click', () => mySiema.ne
 (function($) {
     $(function() {
       
-      $('ul.catalog__tabs').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
+		$('ul.catalog__tabs').on('click', 'li:not(.active)', function() {
+			$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+		});
       
       /* $('.catalog-item__link').each(function(i){
         $(this).on('click', function(e){
@@ -54,25 +54,47 @@ document.querySelector('.slick_next').addEventListener('click', () => mySiema.ne
 
 
 
-      function toggleSlide(item){
-        $(item).each(function(i){
-            $(this).on('click', function(e){
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-          })
-      }
+		function toggleSlide(item){
+			$(item).each(function(i){
+				$(this).on('click', function(e){
+					e.preventDefault();
+					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+				})
+			})
+		}
 
 
-      toggleSlide('.catalog-item__link')
-      toggleSlide('.catalog-item__back')
+		toggleSlide('.catalog-item__link');
+		toggleSlide('.catalog-item__back');
 
 
 
       
       
+      // Modal
 
+		$('[data-modal=consultation]').on('click', function(){
+			$('.overlay, #consultation').fadeIn();
+		});
+		$('.button_mini').on('click', function(){
+			
+			$('.overlay, #order').fadeIn();
+		});
+
+		$('.button_mini').each(function(i){
+			$(this).on('click', function(){
+				$('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
+			})
+		})
+
+		$('.modal__close').on('click', function(){
+			$('.overlay, #order, #consultation, #thanks').fadeOut();
+		});
+		$('.button_modal').on('click', function(){
+			$('#order, #consultation').fadeOut();
+			$('#thanks').fadeIn();
+		});
     });
     })(jQuery);
     
